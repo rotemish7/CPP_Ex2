9efcb829eb111068b34e0830407792b5d80e5009
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-04-16 16:14:18 +0300

    update familytree files

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include "FamilyTree.hpp"
+using namespace std;
 
-int main()
+family::Tree &family::Tree::addFather(string child,string father)
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    return *this;
 }
+
+family::Tree &family::Tree::addMother(string child,string mother)
+{
+    return *this;
+}
+
+string family::Tree::relation(string name)
+{
+    return "unrelated";
+}
+
+string family::Tree::find(string relation)
+{
+    return "find";
+}
+
+bool family::Tree::remove(string name)
+{
+    return true;
+}
+
+void family::Tree::display(){}
+
--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -2,7 +2,44 @@
 // Created by rotem levy on 02/04/2020.
 //
 
-#ifndef ANCESTOR_TREE_FAMILYTREE_H
-#define ANCESTOR_TREE_FAMILYTREE_H
+#ifndef ANCESTOR_TREE_FAMILYTREE_HPP
+#define ANCESTOR_TREE_FAMILYTREE_HPP
+#include <iostream>
 
-#endif //ANCESTOR_TREE_FAMILYTREE_H
+using namespace std;
+
+class node
+{
+    public:
+        string name;
+        node *mother;
+        node *father;
+        node(string name)
+        {
+            (*this).name=name;
+            (*this).mother= nullptr;
+            (*this).father= nullptr;
+        }
+};
+
+namespace family
+{
+    class Tree
+    {
+        public:
+            node *root;
+            Tree(string name)
+            {
+                this->root = new node(name);
+            }
+
+            Tree& addFather(string child,string father);
+            Tree& addMother(string child,string mother);
+            string relation(string name);
+            string find(string relation);
+            bool remove(string name);
+            void display();
+    };
+}
+
+#endif //ANCESTOR_TREE_FAMILYTREE_HPP
