e6e4406f6657e4f19b31a1e0a31dfce2a1d811e2
rotemish7 <57870796+rotemish7@users.noreply.github.com>
2020-04-23 16:43:03 +0300

    test

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -1,31 +1,288 @@
 #include <iostream>
 #include "FamilyTree.hpp"
+
 using namespace std;
 
-family::Tree &family::Tree::addFather(string child,string father)
+namespace family
 {
-    return *this;
-}
+    /**
+     *
+     * @param child
+     * @param father
+     * @return
+     */
+    Tree &family::Tree::addFather(string child, string father)
+    {
+        Tree* temp = search(child, this);
+        if (temp == NULL)
+        {
+            throw runtime_error("No such name");
+        }
+        else if (temp->father != NULL)
+        {
+            throw runtime_error("Already has a father");
+        }
+        else
+        {
+            temp->father = new Tree(father);
+            temp->father->child = temp;
+        }
+        return *this;
+    }
 
-family::Tree &family::Tree::addMother(string child,string mother)
-{
-    return *this;
-}
+    /**
+     *
+     * @param child
+     * @param mother
+     * @return
+     */
+    Tree &family::Tree::addMother(string child, string mother)
+    {
+        Tree* temp = search(child, this);
+        if (temp == NULL)
+        {
+            throw runtime_error("No such name");
+        }
+        else if (temp->father != NULL)
+        {
+            throw runtime_error("Already has a mother");
+        }
+        else
+        {
+            temp->mother = new Tree(mother);
+            temp->mother->child = temp;
+        }
+        return *this;
+    }
 
-string family::Tree::relation(string name)
-{
-    return "unrelated";
-}
+    /**
+     *
+     * @param name
+     * @return
+     */
+    string Tree::relation(string name)
+    {
+        Tree *temp = search(name,this);
+        Tree *ans = temp;
 
-string family::Tree::find(string relation)
-{
-    return "find";
-}
+        if(temp == this)
+        {
+            return "me";
+        }
+        if(temp == NULL)
+        {
+            return "unrelated";
+        }
+        int count = 0;
+        string name_relation  = "";
+        string relation[] = {"", "grand", "great-grand"};
+        while (temp != this)
+        {
+            count++;
+            temp = temp->child;
+        }
+        while(count>3)
+        {
+            rela+="great-";
+            count--;
+        }
+        ans = ans->child;
+        if(ans->father != NULL && ans->father->name.compare(name) == 0)
+        {
+            name_relation+=relation[count-1];
+            return name_relation+="father";
+        }
+        else if(ans->mother != NULL && ans->mother->name.compare(name) == 0)
+        {
+            name_relation+=relation[count-1];
+            return name_relation+="mother";
+        }
+        return NULL;
+    }
 
-bool family::Tree::remove(string name)
-{
-    return true;
-}
+    /**
+     *
+     * @param relation
+     * @return
+     */
+    string Tree::find(string relation)
+    {
+        if(relation.compare("me") == 0)
+        {
+            return this->name;
+        }
+        else if(relation.compare("father") == 0 )
+        {
+            if(this->father != NULL)
+            {
+                return this->father->name;
+            }
+            throw runtime_error("No such relationship");
+        }
+        else if(relation.compare("mother") == 0)
+        {
+            if(this->mother != NULL)
+            {
+                return this->mother->name;
+            }
+            throw runtime_error("No such relationship");
+        }
+        else if(relation.compare("grandmother") == 0)
+        {
+            if(this->father != NULL && this->father->mother != NULL)
+                return this->father->mother->name;
+            else if(this->mother != NULL && this->mother->mother != NULL)
+                return this->mother->mother->name;
+            else throw runtime_error("No such relationship");
+        }
+        else if(relat.compare("grandfather") == 0)
+        {
+            if(this->father != NULL && this->father->father != NULL)
+                return this->father->father->name;
+            else if(this->mother != NULL && this->mother->father != NULL)
+                return this->mother->father->name;
+            else throw runtime_error("No such relationship");
+        }
+        else if(relat.find("great-") == 0)
+        {
+            size_t found = relation.find("great-");
+            int count=3;
+            Tree* temp = this;
+            found = relation.find("great-",found+1,6);
+            while(found != string::npos)
+            {
+                count++;
+                found = relation.find("great-",found+1,6);
+            }
+            if(relation.find("mother") != string::npos)
+            {
+                string str = findH(count, this, 1);
+                if (str == "NULL")
+                {
+                    throw runtime_error("No such relationship");
+                }
+                return str;
+            }
+            else if(relation.find("father") != string::npos)
+            {
+                string str = findH(count, this, 0);
+                if (str == "NULL")
+                {
+                    throw runtime_error("No such relationship");
+                }
+                return str;
+            }
+        }
+        else throw runtime_error("No such relationship");
+        return NULL;
+    }
 
-void family::Tree::display(){}
+    /**
+     *
+     * @param count
+     * @param leaf
+     * @param type
+     * @return
+     */
+    string Tree::findH(int count, Tree* temp, int type)
+    {
+        if(temp == NULL) return "NULL";
+        if(count == 1)
+        {
+            if(type==1 && temp->mother != NULL)
+            {
+                return temp->mother->name;
+            }
+            else if(type==0 && temp->father != NULL)
+            {
+                return temp->father->name;
+            }
+            else return "NULL";
+        }
+        string father_T = findH(count-1, leaf->father, type);
+        string mother_T = findH(count-1, leaf->mother, type);
+        if(mother_T.compare("NULL"))
+        {
+            return mother_T;
+        }
+        return father_T;
+    }
 
+    /**
+     *
+     * @param name
+     * @param node
+     * @return
+     */
+    Tree* Tree::search(string name, Tree* node)
+    {
+        if(node == NULL) return NULL;
+        if(node->name == name) return node;
+        Tree* Father = search(name, node->father);
+        Tree* Mother = search(name, node->mother);
+        if(Father != NULL) return Father;
+        if(Mother != NULL) return Mother;
+        return NULL;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    void Tree::remove(string name)
+    {
+        Tree* temp = search(name, this);
+
+        if(temp == NULL)
+        {
+            throw runtime_error("No such name");
+        }
+        else if(temp->name.compare(this->name) == 0)
+        {
+            throw runtime_error("Root can't be deleted");
+        }
+        else
+        {
+            Tree* temp2 = temp->child;
+            if(temp != NULL)
+            {
+                if(temp2->father != NULL && temp2->father->name == name)
+                {
+                    temp2->father=NULL;
+                }
+                else if (temp2->mother != NULL && temp2->mother->name == name)
+                {
+                    temp2->mother=NULL;
+                }
+                delete temp;
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    void Tree::display()
+    {
+        print2DUtil(this, 0);
+    }
+
+    // https://www.geeksforgeeks.org/print-binary-tree-2-dimensions/
+    /**
+     *
+     * @param root
+     * @param space
+     */
+    void print2DUtil(Tree *root, int space)
+    {
+        if (root == NULL)          // Base case
+            return;
+        space += 10;          // Increase distance between levels
+        print2DUtil(root->father, space);        // Process right child
+        cout << endl;          // Print current node after space
+        for (int i = 10; i < space; i++)
+            cout << " ";
+        cout << root->data << "\n";
+        print2DUtil(root->mother, space);          // Process left child
+    }
+};
